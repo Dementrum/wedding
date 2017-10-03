9b27395a1eb623e623346745da0e4cd5095663ff
Kolesnikov Dmitry - Slash <dmi56756877@mail.ru>
2017-10-03 15:42:15 +0900

    Добавлена рамка кнопке

--- gulp4start-master/gulp4start-master/dev/static/js/main.js
+++ gulp4start-master/gulp4start-master/dev/static/js/main.js
@@ -14,7 +14,21 @@ $(document).ready(function () {
 		}
 		
 	})
+   
 
+   $('a[href^="#"]').click(function () { 
+     elementClick = $(this).attr("href");
+     destination = $(elementClick).offset().top;
+     if($.browser.safari){
+       $('body').animate( { scrollTop: destination }, 1100 );
+     }else{
+       $('html').animate( { scrollTop: destination }, 1100 );
+     }
+     return false;
+   });
+
+
+	
 	$('.reviews-slider-item'). slick({
 		autoplay: true,
 		autoplaySpeed: 700000,
