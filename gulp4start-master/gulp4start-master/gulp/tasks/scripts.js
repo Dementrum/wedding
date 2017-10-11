9b39693b778547a62a5493a6b401a5db04d9ce00
Kolesnikov Dmitry - Slash <dmi56756877@mail.ru>
2017-10-11 18:21:14 +0900

    Пробы

--- gulp4start-master/gulp4start-master/gulp/tasks/scripts.js
+++ gulp4start-master/gulp4start-master/gulp/tasks/scripts.js
@@ -48,4 +48,29 @@ module.exports = function() {
                 stream: true
             }));
     });
+    // Jquery-validator
+    $.gulp.task('libsJS:dev', () => {
+        return $.gulp.src(['node_modules/jquery-validation/dist/jquery.validate.min.js'])
+            .pipe($.gp.concat('libs.min.js'))
+            .pipe($.gulp.dest('./build/static/js/'))
+            .pipe($.browserSync.reload({
+                stream: true
+            }));
+    });
+
+    $.gulp.task('libsJS:build', () => {
+        return $.gulp.src(['node_modules/jquery-validation/dist/jquery.validate.min.js'])
+            .pipe($.gp.concat('libs.min.js'))
+            .pipe($.gp.uglifyjs())
+            .pipe($.gulp.dest('./build/static/js/'));
+    });
+
+    $.gulp.task('js:copy', () => {
+        return $.gulp.src(['./dev/static/js/*.js',
+                           '!./dev/static/js/jquery-validate.min.js'])
+            .pipe($.gulp.dest('./build/static/js/'))
+            .pipe($.browserSync.reload({
+                stream: true
+            }));
+    });
 };
