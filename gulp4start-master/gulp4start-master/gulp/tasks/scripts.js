5703cdf90ff0564560973ea31fa14ed795ed4eb3
Kolesnikov Dmitry - Slash <dmi56756877@mail.ru>
2017-10-11 17:20:29 +0900

    Слайдер в Jquery через Gulp

--- gulp4start-master/gulp4start-master/gulp/tasks/scripts.js
+++ gulp4start-master/gulp4start-master/gulp/tasks/scripts.js
@@ -23,4 +23,29 @@ module.exports = function() {
                 stream: true
             }));
     });
+    // Slick-slider
+    $.gulp.task('libsJS:dev', () => {
+        return $.gulp.src(['node_modules/slick-carousel/slick/slick.min.js'])
+            .pipe($.gp.concat('libs.min.js'))
+            .pipe($.gulp.dest('./build/static/js/'))
+            .pipe($.browserSync.reload({
+                stream: true
+            }));
+    });
+
+    $.gulp.task('libsJS:build', () => {
+        return $.gulp.src(['node_modules/slick-carousel/slick/slick.min.js'])
+            .pipe($.gp.concat('libs.min.js'))
+            .pipe($.gp.uglifyjs())
+            .pipe($.gulp.dest('./build/static/js/'));
+    });
+
+    $.gulp.task('js:copy', () => {
+        return $.gulp.src(['./dev/static/js/*.js',
+                           '!./dev/static/js/slick.min.js'])
+            .pipe($.gulp.dest('./build/static/js/'))
+            .pipe($.browserSync.reload({
+                stream: true
+            }));
+    });
 };
