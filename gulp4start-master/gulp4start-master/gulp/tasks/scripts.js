f637dc9ab32740f5feb8d5273a1eae5afc83bb44
Dementr Slash <dmi56756877@yandex.ru>
2017-10-12 20:13:15 +0900

    Сделано в ноду добавлено

--- gulp4start-master/gulp4start-master/gulp/tasks/scripts.js
+++ gulp4start-master/gulp4start-master/gulp/tasks/scripts.js
@@ -1,81 +1,30 @@
-'use strict';
 module.exports = function() {
     $.gulp.task('libsJS:dev', () => {
-        return $.gulp.src(['node_modules/svg4everybody/dist/svg4everybody.min.js'])
+        return $.gulp.src(['node_modules/svg4everybody/dist/svg4everybody.min.js',
+            'node_modules/jquery-validation/dist/jquery.validate.min.js',
+            'node_modules/slick-carousel/slick/slick.min.js'])
             .pipe($.gp.concat('libs.min.js'))
             .pipe($.gulp.dest('./build/static/js/'))
             .pipe($.browserSync.reload({
                 stream: true
             }));
-    });
+});
 
     $.gulp.task('libsJS:build', () => {
-        return $.gulp.src(['node_modules/svg4everybody/dist/svg4everybody.min.js'])
+        return $.gulp.src(['node_modules/svg4everybody/dist/svg4everybody.min.js',
+            'node_modules/jquery-validation/dist/jquery.validate.min.js',
+            'node_modules/slick-carousel/slick/slick.min.js'])
             .pipe($.gp.concat('libs.min.js'))
             .pipe($.gp.uglifyjs())
             .pipe($.gulp.dest('./build/static/js/'));
-    });
+});
 
     $.gulp.task('js:copy', () => {
         return $.gulp.src(['./dev/static/js/*.js',
-                           '!./dev/static/js/libs.min.js'])
+            '!./dev/static/js/libs.min.js'])
             .pipe($.gulp.dest('./build/static/js/'))
             .pipe($.browserSync.reload({
                 stream: true
             }));
-    });
-
+});
 };
-    // Slick-slider
-    $.gulp.task('libsJS:dev', () => {
-        return $.gulp.src(['node_modules/slick-carousel/slick/slick.min.js'])
-            .pipe($.gp.concat('libs.min.js'))
-            .pipe($.gulp.dest('./build/static/js/'))
-            .pipe($.browserSync.reload({
-                stream: true
-            }));
-    });
-
-    $.gulp.task('libsJS:build', () => {
-        return $.gulp.src(['node_modules/slick-carousel/slick/slick.min.js'])
-            .pipe($.gp.concat('libs.min.js'))
-            .pipe($.gp.uglifyjs())
-            .pipe($.gulp.dest('./build/static/js/'));
-    });
-
-    $.gulp.task('js:copy', () => {
-        return $.gulp.src(['./dev/static/js/*.js',
-                           '!./dev/static/js/slick.min.js'])
-            .pipe($.gulp.dest('./build/static/js/'))
-            .pipe($.browserSync.reload({
-                stream: true
-            }));
-    });
-
-
-    // Jquery-validator
-    $.gulp.task('libsJS:dev', () => {
-        return $.gulp.src(['node_modules/jquery-validation/dist/jquery.validate.min.js'])
-            .pipe($.gp.concat('libs.min.js'))
-            .pipe($.gulp.dest('./build/static/js/'))
-            .pipe($.browserSync.reload({
-                stream: true
-            }));
-    });
-
-    $.gulp.task('libsJS:build', () => {
-        return $.gulp.src(['node_modules/jquery-validation/dist/jquery.validate.min.js'])
-            .pipe($.gp.concat('libs.min.js'))
-            .pipe($.gp.uglifyjs())
-            .pipe($.gulp.dest('./build/static/js/'));
-    });
-
-    $.gulp.task('js:copy', () => {
-        return $.gulp.src(['./dev/static/js/*.js',
-                           '!./dev/static/js/jquery-validate.min.js'])
-            .pipe($.gulp.dest('./build/static/js/'))
-            .pipe($.browserSync.reload({
-                stream: true
-            }));
-    });
-
